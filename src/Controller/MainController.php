64689cc4a6dc1a9ac494ef356b6c20c4d2da4b71
akramsl <akram113@hotmail.fr>
2023-10-10 12:05:28 +0200

    redirection du lien du logo vers l'acceuil

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    #[Route('', name: 'acceuil')]
+    #[Route('/', name: 'acceuil')]
     public function index(): Response
     {
         return $this->render('main/index.html.twig', [
